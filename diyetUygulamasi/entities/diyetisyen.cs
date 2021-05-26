4c301026d5cc3f9ef69892166681ea9959f91cf9
Cüneyt <ugurongun@hotmail.com>
2021-05-26 21:11:15 +0300

    Add project files.

---
+++ diyetUygulamasi/entities/diyetisyen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diyetUygulamasi.entities
+{
+    public class diyetisyen : kisi
+    {
+        public string kullaniciAdi { get; set; }
+        public string sifre { get; set; }
+
+        public List<hasta> hastalar { get; set; }
+
+
+        public void hastaEkle(hasta hasta)
+        {
+            if (hastalar != null)
+            {
+                hastalar.Add(hasta);
+            }
+            else
+            {
+                hastalar = new List<hasta>();
+                hastalar.Add(hasta);
+            }
+
+
+        }
+    }
+}
